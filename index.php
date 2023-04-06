41e9087981e6efd88df7546b10ae4f3d08f066a4
Sara <122261646+SaraCrds@users.noreply.github.com>
2023-04-06 16:09:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The District</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets\css\style.css">
+</head>
+<body class="container bg-peach">
+    <div class="d-flex pt-2 justify-content-center">
+        <h1>Nom de l'Enseigne</h1>
+    </div>
+        <div class="row no-gutters justify-content-center align-content-center test">
+            <div class="pb-2 d-flex justify-content-center">
+                <h2>Nos produits phares</h2>
+            </div>
+            <div class="card col-3 mx-2">
+                <img src="#" class="card-img-top" alt="#">
+                <div class="card-body">
+                    <h5 class="card-title">Nom produit</h5>
+                    <p class="card-text">Description Produit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, quibusdam!</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><a href="">En savoir plus...</a></li>
+                </ul>
+            </div>
+            <div class="card col-3 mx-2">
+                <img src="#" class="card-img-top" alt="#">
+                <div class="card-body">
+                    <h5 class="card-title">Nom produit</h5>
+                    <p class="card-text">Description Produit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, quibusdam!</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><a href="">En savoir plus...</a></li>
+                </ul>
+            </div>
+            <div class="card col-3 mx-2">
+                <img src="#" class="card-img-top" alt="#">
+                <div class="card-body">
+                    <h5 class="card-title">Nom produit</h5>
+                    <p class="card-text">Description Produit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, quibusdam!</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><a href="">En savoir plus...</a></li>
+                </ul>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
