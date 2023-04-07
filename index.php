2a5fcfd87357adffe53a4b07d8c6b3835f0cfb2c
SaraCrds <sara.decastro@outlook.com>
2023-04-07 12:19:29 +0200

    update

--- index.php
+++ index.php
@@ -8,8 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="assets\css\style.css">
 </head>
-<body class="container bg-peach">
-    <div class="d-flex pt-2 justify-content-center">
+<body class="body-bg bg-peach m-0 p-0">
+    <nav class="navbar sticky-top d-flex justify-content-between bg-warning w-100">
+    <div class="px-2 col-4">Flex item L</div>
+    <div class="px-2 col-4 text-center display-6">Nom de l'Enseigne</div>
+    <div class="px-2 col-4"></div>
+</nav>
+    <div class="pt-2">
         <h1>Nom de l'Enseigne</h1>
     </div>
         <div class="row no-gutters justify-content-center align-content-center test">
@@ -47,6 +52,9 @@
                 </ul>
             </div>
         </div>
+        <br><br><br>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae, adipisci esse voluptate voluptatibus eligendi exercitationem, autem vel eaque magnam dolorem dignissimos optio totam dicta laudantium! Voluptatibus excepturi at mollitia fugit vero culpa nam ut quo odio sit officia eos quisquam hic quos iusto earum et qui iure, expedita nulla tenetur eveniet id optio repellendus? Asperiores voluptatem, accusamus sunt necessitatibus quae, eum earum ducimus culpa atque aut repellat ut maiores quidem fuga quisquam officia aliquam! Eaque excepturi voluptates, sint porro necessitatibus fugiat reiciendis debitis impedit quod incidunt delectus omnis consectetur odit deleniti suscipit, fugit blanditiis illum perspiciatis odio? Porro, minus! Officiis, iste delectus reprehenderit repellendus culpa sequi labore consectetur alias dicta natus. Blanditiis dicta dolorem sequi facere doloribus voluptatum, ullam odio minus doloremque, esse neque eveniet similique? Voluptatem, modi. Quas aut minima facere necessitatibus fugiat perspiciatis. Itaque saepe laudantium, dolores nemo laboriosam incidunt eum iure, earum nostrum, consequuntur adipisci ut sunt dolore quaerat sint impedit numquam illum nam culpa natus optio. Similique veniam error atque repellat animi explicabo incidunt, libero beatae ea sunt eveniet assumenda ex deleniti ad consequuntur a corporis voluptatibus. Asperiores impedit dolore minus iusto debitis consequatur eaque reiciendis sequi, maxime, aut blanditiis corporis odio. Itaque, aperiam nisi animi reprehenderit, ut repellat soluta recusandae, non fugit veritatis esse sed? Fugit repellendus molestias delectus illum ratione vel dolorem eaque sequi? Est soluta repudiandae, quaerat obcaecati impedit a cupiditate quasi deserunt nihil! Maxime, illo excepturi, obcaecati molestias voluptatibus ipsam saepe voluptates rem vitae quia aliquid! Mollitia consequatur vero dolorum fuga delectus, dignissimos obcaecati! Quam mollitia quibusdam quis iusto, sapiente omnis eveniet nesciunt sequi accusantium culpa laborum. Minima aspernatur adipisci iure porro omnis amet optio doloribus deserunt esse nemo, reprehenderit rem quibusdam voluptatibus et repellendus velit distinctio natus laudantium. Maiores, fugiat delectus facere atque earum adipisci veniam nostrum laborum qui dolores error! Ipsam blanditiis, atque, eaque quia cupiditate voluptatem dolorum molestiae recusandae, perferendis quod nisi deserunt aspernatur rem laboriosam unde fugiat officiis animi officia magnam. Veniam dolorem esse dignissimos, reprehenderit, consequuntur, obcaecati recusandae reiciendis ipsum excepturi tempore unde libero. Molestiae quidem consequatur eveniet dicta odit, ea dolorum tempora sunt? Sit enim corporis asperiores laborum voluptatibus tempora nulla fuga adipisci accusamus nobis perferendis possimus eligendi, autem voluptate recusandae libero sapiente doloribus, corrupti dolorem dolore vel culpa. Officia, tempora nam dicta error corrupti aut non deserunt ea nulla blanditiis odio aliquam saepe explicabo consequuntur nisi quam eveniet necessitatibus laborum similique. Et voluptate modi distinctio vero natus recusandae vitae numquam harum? Eaque porro natus expedita vel ea ad corporis. Facilis illo magnam ullam ratione aperiam assumenda qui. Iusto quasi reprehenderit nisi non hic tempore doloremque tempora, eius voluptates cum fugiat laboriosam rerum corporis officiis maiores! Culpa accusantium magnam corporis dicta repudiandae eius dolore aperiam, distinctio, qui hic voluptates! Ipsam cumque sunt aspernatur est magni architecto voluptates nobis illum odit sit exercitationem, nostrum in nesciunt optio deserunt ut sed dicta fugit perspiciatis facere unde. Ipsum ipsam earum similique facilis itaque ipsa saepe. Autem, eum itaque eaque a repellendus delectus tempore et quae, voluptas, tenetur in dolorum!</p>
+        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
