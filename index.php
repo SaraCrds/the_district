51170bdf73b09df17570df3f75876568aa3b80d8
SaraCrds <sara.decastro@outlook.com>
2023-04-11 16:56:33 +0200

    update 1104

--- index.php
+++ index.php
@@ -7,16 +7,54 @@
     <title>The District</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
-<body class="body-bg bg-peach m-0 p-0">
-    <nav class="navbar sticky-top d-flex justify-content-between bg-warning w-100">
-    <div class="px-2 col-4">Flex item L</div>
-    <div class="px-2 col-4 text-center display-6">Nom de l'Enseigne</div>
-    <div class="px-2 col-4"></div>
-</nav>
-    <div class="pt-2">
-        <h1>Nom de l'Enseigne</h1>
+<body class="body-bg bg-black m-0 p-0">
+<nav class="navbar navbar-expand-lg sticky-top navbar bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+        </li>
+        <li class="text-primary align-content-center">
+        <span class="material-symbols-outlined">
+            magic_button
+        </span>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Catégorie
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Plats
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Contact</a>
+        </li>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
         <div class="row no-gutters justify-content-center align-content-center test">
             <div class="pb-2 d-flex justify-content-center">
                 <h2>Nos produits phares</h2>
